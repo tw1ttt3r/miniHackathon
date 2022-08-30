a90fb384f452fad1ee77aeef92aac154902e19f1
tw1ttt3r <tw1ttt3r@gmail.com>
2022-08-30 10:22:42 -0500

    feat: added filter functionality and delete consoles

--- js/utils.js
+++ js/utils.js
@@ -5,7 +5,7 @@ function fetchData(endpoint) {
       cnf.container.innerHTML = '';
       cnf.allData = results;
       cnf.displayResults.innerHTML = results.length;
-      console.log("display", results)
+      renderFilters()
       if (!cnf.selected) {
         renderResults();
         return
@@ -35,7 +35,52 @@ function renderResults() {
     });
 }
 
-function renderResult(name, image) {
+function renderResult(name, image, container = cnf.container) {
   const component = `<section><p>${name}</p><img src="${image}"></section>`;
-    cnf.container.insertAdjacentHTML('beforeend', component)
+  container.insertAdjacentHTML('beforeend', component)
+}
+
+function renderFilters() {
+  cnf.containerFilters.innerHTML = '';
+  cnf.filters[cnf.selected]
+    .forEach( ({field: filter}) => {
+      cnf.containerFilters.insertAdjacentHTML('beforeend', `<input type="checkbox" class="filterData" name="${filter}"> <label for="${filter}">${filter}</label>`)
+    })
+  Array.from(document.querySelectorAll(".filterData"))
+    .forEach( element => {
+      element.addEventListener("click", ({ target: { name } }) => {
+        if (cnf.filterSelected.includes(name)) {
+          cnf.filterSelected =  cnf.filterSelected.filter( filter => filter !== name )
+          return
+        }
+        cnf.filterSelected = [ ...cnf.filterSelected, name ]
+      })
+    })
+}
+
+function renderFilterResults() {
+  const results = cnf.filterSelected.reduce((prev, curr, i) => {
+  const field = cnf.filters[cnf.selected].filter( filterr => filterr.field === curr )[0].inData
+    if (!i) {
+      prev = cnf.allData.filter( registro => registro[field].includes(cnf.textFilter))
+      return prev
+    }
+    return prev.filter( registro => registro[field] === cnf.textFilter )
+  }, [])
+  cnf.displayResultsFilter.innerHTML = '';
+  cnf.displayErrorFilter.innerHTML = ''
+  if (!results.length) {
+    cnf.displayErrorFilter.innerHTML = 'Sin registros encontrados'
+    return
+  }
+  results.forEach( registro => renderResult(registro.name, !cnf.selected ? registro.image : registro.sprites.front_default, cnf.displayResultsFilter) )
+}
+
+function beginFilter(search) {
+  if (!cnf.filterSelected.length) {
+    cnf.displayError.innerHTML = "No existe un filtro seleccionado"
+    return
+  }
+  cnf.displayError.innerHTML = ""
+  if (search) renderFilterResults()
 }
